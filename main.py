d69988349e1c6b4459ae4864c985ee3fb6b1f423
Mateus Gutierrez <118942632+MateusGutierrez@users.noreply.github.com>
2025-02-09 17:32:39 -0300

    add cord and middleware

--- main.py
+++ main.py
@@ -4,10 +4,24 @@ from pydantic import BaseModel
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
-from dummy import dummy 
+from dummy import dummy
+from fastapi.middleware.cors import CORSMiddleware 
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
+
+origins = [
+    "http://localhost:3000",  # React em desenvolvimento
+    "http://127.0.0.1:3000",
+    "https://meusite.com",  # Produção
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Permitir apenas esses domínios
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos os métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permitir todos os headers
+)
 
 def inserir_acomodacoes(db: Session):
     if db.query(models.Acomodacoes).count() == 0:
@@ -55,7 +69,6 @@ async def get_acomodacoes(
     query = db.query(models.Acomodacoes)
     if cidade:
         query = query.filter(models.Acomodacoes.localizacao.ilike(f"%{cidade}%"))
-        print(query.all())
     return query.all()
 
 @app.get("/acomodacoes/{id}", response_model=Acomodacoes)
